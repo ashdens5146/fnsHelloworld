528bdcc13fd420a69135d0ba83d477b67d056220
ashdens5146 <ashdens84@gmail.com>
2021-04-27 11:01:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
-var router = express.Router();
-router.get('/',function(req,res){
-  res.json({"error" : false, "message" : "Hello World!"});
-});
-router.post('/add',function(req,res){
-  res.json({"error" : false, "message" : "success", "data" :  parseInt(parseInt(req.body.num1) + parseInt(req.body.num2))});
-});
-app.use('/',router);
-app.listen(80,function(){
-  console.log("Listening at PORT 80");
+const fdk=require('@fnproject/fdk');
+
+fdk.handle(function(input){
+  let name = 'World';
+  if (input.name) {
+    name = input.name;
+  }
+  console.log('\nInside Node Hello World function')
+  return {'message': 'Hello ' + name}
 })
