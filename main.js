79050001cb66d1728a53a3e65ceca912848606ae
ashdens5146 <ashdens84@gmail.com>
2021-05-10 11:23:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const fdk=require('@fnproject/fdk');
 
 fdk.handle(function(input){
-  let name = 'Ashish is the Best Developer';
+  let name = 'Ashish';
   if (input.name) {
     name = input.name;
   }
   console.log('\nInside Node Hello World function')
   //return {'message': 'Hello ' + name}
-  return { 'userId': 1, 'id': 1,'title': 'Functions and Jenkins','completed': false }
+  return { 'userId': 1, 'id': 1,'title': 'Functions and Jenkins Demo','completed': false }
 })
